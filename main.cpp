6f7fa5fc90f08e30b15adfe3d37974f38a7c9860
verden <155012457+verdeneeee@users.noreply.github.com>
2026-05-08 18:18:47 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include "raylib/include/raylib.h"
 #include "src/player.h"
+#include "src/world.h"
 
 int main()
 {
-    InitWindow(800, 450, "hello world");
+    InitWindow(1800, 900, "hello world");
+    SetTargetFPS(60);
 
     Player player({ GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f });
+    World world;
+
+    Camera2D cam = { 0 };
+    cam.target = { player.position.x, player.position.y - 30.0f};
+    cam.offset = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+    cam.rotation = 0.0f;
+    cam.zoom = 1.0f;
+
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(SKYBLUE);
-
         float deltaTime = GetFrameTime();
+        Vector2 mousePos = GetMousePosition();
+        Vector2 worldMousePos = GetScreenToWorld2D(mousePos, cam);
 
-        DrawRectangle(0, (450 + 80) / 2, 800, 450/2, GREEN);
+        cam.target = { player.position.x, player.position.y - 30.0f };
 
-        player.update();
-        player.move(deltaTime);
-        player.jump(deltaTime);
+        BeginDrawing();
+            ClearBackground(SKYBLUE);
+                BeginMode2D(cam);
 
+                player.update(deltaTime);
+                world.update(worldMousePos);
+
+            EndMode2D();
         EndDrawing();
     }
 
     CloseWindow();
 
-	return 0;
+    return 0;
 }
