40ae422f7f0504ec25815b1ba085dbef9138385f
verden <155012457+verdeneeee@users.noreply.github.com>
2026-05-19 18:23:27 +0300

    Update entity.h

--- src/entity.h
+++ src/entity.h
@@ -5,9 +5,16 @@ using EntityID = int64_t;
 
 struct entity
 {
+
+
+
+
+	
+
 	
 	
 	
+	std::vector<Vector2> placedBlocks;
 
 	//Rectangle playerRect = { position.x, position.y, 20, 40 };
 
@@ -15,51 +22,80 @@ struct entity
 
 struct Position
 {
-	Vector2 position = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
+	Vector2 position /* { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f }*/;
 };
 
 struct Velocity
 {
-	Vector2 velocity = { 0, 0 };
+	Vector2 velocity;
 };
 
 struct PlayerSize
 {
-	const float playerHeight = 40.0f;
-	const float playerWidth = 20.0f;
+	const float playerHeight;
+	const float playerWidth;
 };
 
 struct BlockRectangle
 {
-	Rectangle blockRect = { 0 };
+	Rectangle blockRect;
 };
 
 struct Gravity
 {
-	const float gravity = 256.0f;
+	const float gravity/* = 256.0f*/;
 };
 
 struct MaxSpeed
 {
-	const float maxSpeed = 150.0f;
+	const float maxSpeedX /*= 150.0f*/;
+	const float maxSpeedY /*= -150.0f*/;
 };
 
 struct JumpForce
 {
-	const float jumpForce = 130.0f;
+	const float jumpForce /*= 130.0f*/;
 };
 
 struct IsGrounded
 {
-	bool isGrounded = false;
+	bool isGrounded /*= false*/;
 };
 
 struct Cellsize
 {
-	const float cellSize = 20.0f;
+	const float cellSize /*= 20.0f*/;
 };
 
 struct GroundLevel
 {
+	float groundLevel /*= GetScreenHeight() - 90.0f*/;
+};
 
+template <typename Type>
+using ComponentMap = std::unordered_map<EntityID, Type>;
+
+using Positions = ComponentMap<Position>;
+using Velocities = ComponentMap<Velocity>;
+using PlayerSizes = ComponentMap<PlayerSize>;
+using blockRectangle = ComponentMap<BlockRectangle>;
+using gravity = ComponentMap<Gravity>;
+using MaxSpeeds = ComponentMap<MaxSpeed>;
+using jumpForce = ComponentMap<JumpForce>;
+using isGrounded = ComponentMap<isGrounded>;
+using CellSize = ComponentMap<Cellsize>;
+using groundLevel = ComponentMap<GroundLevel>;
+
+struct Components
+{
+	Positions positions;
+	Velocities velocities;
+	PlayerSizes playersizes;
+	blockRectangle BlockRect;
+	gravity Gravity;
+	MaxSpeeds maxSpeeds;
+	jumpForce jumpforce;
+	isGrounded IsGrounded;
+	CellSize cellSize;
+	groundLevel GroundLevel;
 };
